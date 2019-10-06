2708aca023047850abff17c3373373427d254c48
Tim Burns <burnstimothy87@gmail.com>
2019-10-06 02:02:16 -0400

    no now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
+// letter the user types
 const computerChoices= ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-
+// setting for 0
 let wins = 0;
 let losses = 0;
 let guessesLeft = 9;
@@ -8,7 +9,7 @@ let letterUser= [];
 let eachofLetters= null;
 console.log(computerChoices)
 
-
+// setting the computerGuess variable eqaul to computerChoice array
 let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 console.log(Math)
 
@@ -23,13 +24,14 @@ function farUserGuesses(){
 }
 
 
-
+//function will reset everything after 9 choices
 let restart = function(){
     guessesLeft = 9;
     letterUser = [];
     let computerGuess= computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
+//the users key will be selected once it is release
 document.onkeyup = function(event){
     guessesLeft--;
 
